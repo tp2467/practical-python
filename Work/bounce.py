203fda7bb44a4c04e4b52e36abb54e4e4f75341f
Thinh Pham <truongthinh.pham@gmail.com>
2023-07-31 21:19:52 -0400

    Solve 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100.0 # meters
+
+bounce = 1
+while bounce < 11:
+    height = height * 3/5
+    print(bounce, round(height, 4))
+    bounce = bounce + 1
+    
