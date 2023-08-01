f2c3dead2f632b1e7b093be34ba21ffdc811d5b4
Thinh Pham <truongthinh.pham@gmail.com>
2023-07-31 21:55:44 -0400

    Solve 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,14 +1,20 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.7, 1.8
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
 
-while principal > 0 and month <= 12:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-
+while principal > 0:
+    if month < 12:
+        principal = principal * (1+rate/12) - payment - 1000.0
+        total_paid = total_paid + payment + 1000.0
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    month = month + 1
 print('Total paid', total_paid)
+print('Total months', month)
