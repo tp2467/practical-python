578220064e635d2483653f8fd324060e5d3b5926
Thinh Pham <truongthinh.pham@gmail.com>
2023-08-12 10:51:04 -0400

    Solve 2.4 exercise

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,24 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Import a portfolio from filename and return a list of holdings'''
+
+    with open(filename) as f:
+        rows = csv.reader(f)
+        next(rows)
+        mainport = []
+        for row in rows:
+            stock, nshare, price = row[0], int(row[1]), float(row[1].strip())
+            mainport.append((stock, nshare, price))
+    
+    return mainport
+
+
+if __name__ == '__main__':
+    myport = read_portfolio('Data/portfolio.csv')
+    for holding in myport:
+        print(holding)
