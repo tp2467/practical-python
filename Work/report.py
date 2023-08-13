694da61bec84c686da4dbfe72389fe0e5359a6fc
Thinh Pham <truongthinh.pham@gmail.com>
2023-08-13 11:02:57 -0400

    Solve string format exercise 2.9 and 2.10

--- Work/report.py
+++ Work/report.py
@@ -50,8 +50,31 @@ def read_prices(filename):
         
     return price_dict
 
+
+def make_report(portfolio, prices):
+    '''
+    Generate the stock, number of shares, original price, and price change
+    of all stocks in the porfolio.
+    Input:
+        portfolio: list of dictionary of the form {'name':stock, 'shares':num_shares, 'price':price}  }
+        price: dictionary of price of the form {'stock_symbol': price}
+    '''
+    
+    report = []
+    for holding in portfolio:
+        report.append(
+            (holding['name'], holding['shares'], holding['price'], 
+             prices[holding['name']] - holding['price']
+            )
+        )
+    
+    return report
+
+
 if __name__ == '__main__':
     from pprint import pprint
+    
+    ''' ----------- 2.6 and 2.5 exercises
     myport = read_portfolio('Data/portfolio.csv')
     currentprices = read_prices('Data/prices.csv')
 
@@ -64,11 +87,19 @@ if __name__ == '__main__':
     gainloss = 0
     for holding in myport:
         gainloss += holding['shares'] * (currentprices[holding['name']] - holding['price'])
-    
-    
-
 
     print()
     print('Current gain/loss: ', gainloss)
 
+    # ----------- 2.6 and 2.5 exercises ends
+    '''
 
+
+    #Exercise 2.10
+    portfolio = read_portfolio('Data/portfolio.csv')
+    prices = read_prices('Data/prices.csv')
+    report = make_report(portfolio, prices)
+
+    for stock, shares, orgprice, change in report:
+        print(f'{stock:>10s} {shares:>10d} {orgprice:>10.2f} {change:>10.2f}')
+            
