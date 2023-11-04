1c8255005f10fd791539c88e753d323909dc6ec2
Thinh Pham <truongthinh.pham@gmail.com>
2023-11-04 16:32:24 -0400

    Modify report to use fileparse module

--- Work/report.py
+++ Work/report.py
@@ -1,62 +1,42 @@
-# report.py
-#
-# Exercise 2.4
+# Create a report for a portfolio of stocks.
 
-import csv
 import sys
+sys.path.append('/home/thinh/Documents/practical-python/Work')
+import fileparse
 
-# def read_portfolio(filename):
-#     '''Import a portfolio from filename and return a list of holdings
-#        Exercise 2_4
-#     '''
-
-#     with open(filename) as f:
-#         rows = csv.reader(f)
-#         next(rows)
-#         mainport = []
-#         for row in rows:
-#             stock, nshare, price = row[0], int(row[1]), float(row[2].strip())
-#             mainport.append((stock, nshare, price))
-    
-#     return mainport
-
-def read_portfolio(filename):
-    '''Import a portfolio from filename and return a dictionary of holdings
-       Exercise 2_5
+def read_portfolio(filename: str) -> dict:
+    '''Import a portfolio from filename and return a list of dictionaries
+       Each dictionary has keys: 'name', 'shares', and 'price'.
     '''
-
-    with open(filename) as f:
-        rows = csv.reader(f)
-        header = next(rows)
-        mainport = []
-        for row in rows:
-            record = dict(zip(header, row))
-            stock, nshare, price = record['name'], int(record['shares']), float(record['price'].strip())
-            mainport.append({
-                'name':stock,
-                'shares':nshare,
-                'price':price}
-                )  
+    
+    mainport = fileparse.parse_csv(
+        filename=filename,
+        types=[str, int, float]
+    )    
     
     return mainport
 
 
-def read_prices(filename):
-    with open(filename, 'rt') as pricefile:
-        rows = csv.reader(pricefile)
+def read_prices(filename: str) -> dict:
+    '''
+    Read prices from a file name, return a ditionary of stock names and 
+    their prices.
+    '''
     
-        price_dict = {}
-        for row in rows:
-            if len(row) == 0: continue
-            price_dict[row[0]] = float(row[1])
+    price_list = fileparse.parse_csv(
+        filename=filename,
+        types=[str, float],
+        has_headers=False
+    )
+    price_dict = dict(price_list)
         
     return price_dict
 
 
-def make_report(portfolio, prices):
+def make_report(portfolio:list, prices:dict) ->list:
     '''
-    Generate the stock, number of shares, original price, and price change
-    of all stocks in the porfolio.
+    Generate a list of the stock, number of shares, original price, and price 
+    change of all stocks in the porfolio.
     Input:
         portfolio: list of dictionary of the form {'name':stock, 'shares':num_shares, 'price':price}  }
         price: dictionary of price of the form {'stock_symbol': price}
@@ -64,13 +44,38 @@ def make_report(portfolio, prices):
     
     report = []
     for holding in portfolio:
+        price_change = prices[holding['name']] - holding['price']
         report.append(
-            (holding['name'], holding['shares'], prices[holding['name']], 
-             prices[holding['name']] - holding['price']
+            (holding['name'], 
+             holding['shares'], 
+             prices[holding['name']], 
+             price_change
             )
         )
     
     return report
+
+
+def print_report(report):
+    '''
+    Print out all positions and their cost and profit.
+    Args
+        report: list of tuples of holdings.
+    '''
+    headers = ('Name', 'Shares', 'Price', 'Change')
+
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ' )*len(headers) )
+    for record in report:
+        print('%10s %10s %10.2f %10.2f' % record)
+
+
+def portfolio_report(portfolio_filename, price_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    price_list = read_prices(price_filename)
+    report = make_report(portfolio, price_list)
+    print_report(report)
+
 
 if len(sys.argv) == 2:
     portfolio_file = sys.argv[1]
@@ -78,38 +83,4 @@ else:
     portfolio_file = 'Data/portfolio.csv' 
 
 if __name__ == '__main__':
-    from pprint import pprint
-    
-    # ----------- 2.6 and 2.5 exercises
-    # myport = read_portfolio('Data/portfolio.csv')
-    # currentprices = read_prices('Data/prices.csv')
-
-    # print('My portfolio:')
-    # pprint(myport)
-    # print()
-    # print('Current prices')
-    # pprint(currentprices)
-
-    # gainloss = 0
-    # for holding in myport:
-    #     gainloss += holding['shares'] * (currentprices[holding['name']] - holding['price'])
-
-    # print()
-    # print('Current gain/loss: ', gainloss)
-
-    #  ----------- 2.6 and 2.5 exercises ends
-    
-
-
-    #Exercise 2.10, 2.11, 2.12
-    portfolio = read_portfolio(portfolio_file)
-    prices = read_prices('Data/prices.csv')
-    report = make_report(portfolio, prices)
-
-    headers = ('Name', 'Shares', 'Price', 'Change')
-
-    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
-    print(f'{"":->10s} {"":->10s} {"":->10s} {"":->10s}')
-    for stock, shares, orgprice, change in report:
-        print(f'{stock:>10s} {shares:>10d} {"$"+f"{orgprice:.2f}":>10s} {change:>10.2f}')
-            
+    portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
